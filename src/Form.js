8055fd1e39beb0e138c5199b664c88a60945d964
gabri <gabrielescano.bcn@gmail.com>
2021-11-10 16:08:12 +0100

    Add useForm

--- src/Form.js
+++ src/Form.js
@@ -1,25 +1,49 @@
 import { Form, Button } from 'semantic-ui-react';
+import { useForm } from "react-hook-form";
+
 
 export default function FormValidation() {
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const onSubmit = (data) => {
+        console.log(data);
+    }
     return (
-        <Form>
-            <Form.Field>
-                <label>First Name</label>
-                <input placeholder='First Name' type="text" />
-            </Form.Field>
-            <Form.Field>
-                <label>Last Name</label>
-                <input placeholder='Last Name' type="text" />
-            </Form.Field>
-            <Form.Field>
-                <label>Email</label>
-                <input placeholder='Email' type="email" />
-            </Form.Field>
-            <Form.Field>
-                <label>Password</label>
-                <input placeholder='Password' type="password" />
-            </Form.Field>
-            <Button type='submit'>Submit</Button>
-        </Form>
+        <div>
+            <Form onSubmit={handleSubmit(onSubmit)}>
+                <Form.Field>
+                    <label>First Name</label>
+                    <input
+                        placeholder='First Name'
+                        type="text"
+                        {...register("firstName")}
+                    />
+                </Form.Field>
+                <Form.Field>
+                    <label>Last Name</label>
+                    <input
+                        placeholder='Last Name'
+                        type="text"
+                        {...register("lastName")}
+                    />
+                </Form.Field>
+                <Form.Field>
+                    <label>Email</label>
+                    <input
+                        placeholder='Email'
+                        type="email"
+                        {...register("email")}
+                    />
+                </Form.Field>
+                <Form.Field>
+                    <label>Password</label>
+                    <input
+                        placeholder='Password'
+                        type="password"
+                        {...register("password")}
+                    />
+                </Form.Field>
+                <Button type='submit'>Submit</Button>
+            </Form>
+        </div>
     )
 }
