6ebf6051d595ce56eb14489369a05e27a33d192c
gabri <gabrielescano.bcn@gmail.com>
2021-11-10 16:24:59 +0100

    Add validation errors

--- src/Form.js
+++ src/Form.js
@@ -1,7 +1,6 @@
 import { Form, Button } from 'semantic-ui-react';
 import { useForm } from "react-hook-form";
 
-
 export default function FormValidation() {
     const { register, handleSubmit, formState: { errors } } = useForm();
     const onSubmit = (data) => {
@@ -15,33 +14,44 @@ export default function FormValidation() {
                     <input
                         placeholder='First Name'
                         type="text"
-                        {...register("firstName")}
+                        {...register("firstName", { required: true, maxLength: 10 })}
                     />
                 </Form.Field>
+                {errors.firstName && <p>Please check the First Name</p>}
                 <Form.Field>
                     <label>Last Name</label>
                     <input
                         placeholder='Last Name'
                         type="text"
-                        {...register("lastName")}
+                        {...register("lastName", { required: true, maxLength: 10 })}
                     />
                 </Form.Field>
+                {errors.lastName && <p>Please check the Last Name</p>}
                 <Form.Field>
                     <label>Email</label>
                     <input
                         placeholder='Email'
                         type="email"
-                        {...register("email")}
+                        {...register("email",
+                            {
+                                required: true,
+                                pattern: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+                            })}
                     />
                 </Form.Field>
+                {errors.email && <p>Please check the Email</p>}
                 <Form.Field>
                     <label>Password</label>
                     <input
                         placeholder='Password'
                         type="password"
-                        {...register("password")}
+                        {...register("password", {
+                            required: true,
+                            pattern: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,15}$/
+                        })}
                     />
                 </Form.Field>
+                {errors.password && <p>Please check the Password</p>}
                 <Button type='submit'>Submit</Button>
             </Form>
         </div>
